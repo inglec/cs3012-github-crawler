a9c40de0215cd73e4c93ee416fa46b3388e3016e
inglec <inglec@tcd.ie>
2017-11-23 11:19:30 +0000

    Migrated to Bootstrap v4. Added Open Iconic library for icons

--- public/js/github-insite.js
+++ public/js/github-insite.js
@@ -1,32 +1,31 @@
 var username;
 var token;
+var userData;
 
 window.onload = function() {
     username = sessionStorage.getItem('username');
     token = sessionStorage.getItem('token');
 
-    $('#welcome-user').text("Welcome " + username + "!");
+    showHome();
+    $('#dropdown').text(username);
+
+    userData = getUserData();
 }
 
-function getUser() {
+function getUserData() {
     $.ajax({
         url: "https://api.github.com/users/" + username,
         method: "GET",
+        data: {
+            "access_token": token
+        },
         success: function(data, textStatus, jqXHR) {
             console.log("Successfully fetched user data:");
             console.log(data);
 
-            var html = '<h1><img src=' + data.avatar_url + ' style="width:60px; height:60px"></img> '+ username + '</h1>';
-            html += '<table class="table">';
-            $.each(data, function(key, value){
-                html += '<tr>';
-                html += '<td>' + key + '</td>';
-                html += '<td>' + value + '</td>';
-                html += '</tr>';
-            });
-            html += '</table>';
+            $('#user-github-link').attr("href", data.html_url);
 
-            $("#results").html(html);
+            userData = data;
         },
         error: function(jqXHR, textStatus, errorThrown) {
             console.log(errorThrown);
@@ -34,7 +33,7 @@ function getUser() {
     });
 }
 
-function getRepos() {
+function showRepos() {
     $.ajax({
         url: "https://api.github.com/user/repos",
         method: "GET",
@@ -66,4 +65,25 @@ function getRepos() {
             }
         }
     });
+}
+
+function showUserData() {
+    var html = '<h1><img src=' + userData.avatar_url + ' style="width:60px; height:60px"></img> '+ username + '</h1>';
+    html += '<table class="table">';
+    $.each(userData, function(key, value){
+        html += '<tr>';
+        html += '<td>' + key + '</td>';
+        html += '<td>' + value + '</td>';
+        html += '</tr>';
+    });
+    html += '</table>';
+
+    $("#results").html(html);
+}
+
+function showHome() {
+    var html = '<h1 id="welcome-user" class="display-3">Welcome, ' + username + '!</img></h1>';
+    html += '<p class="lead">Use the tabs above to display various analytics about your GitHub account.</p>';
+
+    $('#results').html(html);
 }
